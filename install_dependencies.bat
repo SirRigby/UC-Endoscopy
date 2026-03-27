a41f7de1d0e822ca344eb2106023887928a5f3ca
SirRigby <vanshkunwarji41@gmail.com>
2026-03-27 13:06:01 +0530

    model upload

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @echo off
 
 python -m pip install --upgrade pip
@@ -22,4 +23,30 @@ pip install pathlib
 pip install json5
 
 echo All packages installed successfully!
+=======
+@echo off
+
+python -m pip install --upgrade pip
+
+
+pip install opencv-python
+
+
+pip install tqdm
+
+
+pip install scikit-image
+
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+REM Install PIL / Pillow
+pip install pillow
+
+
+pip install pathlib
+pip install json5
+
+echo All packages installed successfully!
+>>>>>>> 5cae4fc (model upload)
 pause
