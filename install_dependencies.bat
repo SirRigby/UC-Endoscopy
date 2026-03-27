6dd6a9177f49147def5ef2a2c3c92bbee486ee39
SirRigby <vanshkunwarji41@gmail.com>
2026-03-27 13:19:44 +0530

    instll depndcs

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,52 +1,21 @@
-<<<<<<< HEAD
 @echo off
+echo Installing required Python packages...
+
 
 python -m pip install --upgrade pip
 
 
-pip install opencv-python
+pip install torch torchvision torchaudio
+
+
+pip install opencv-python pillow
 
 
 pip install tqdm
 
 
-pip install scikit-image
+pip install numpy
 
-
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-
-REM Install PIL / Pillow
-pip install pillow
-
-
-pip install pathlib
-pip install json5
-
-echo All packages installed successfully!
-=======
-@echo off
-
-python -m pip install --upgrade pip
-
-
-pip install opencv-python
-
-
-pip install tqdm
-
-
-pip install scikit-image
-
-
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-
-REM Install PIL / Pillow
-pip install pillow
-
-
-pip install pathlib
-pip install json5
-
-echo All packages installed successfully!
->>>>>>> 5cae4fc (model upload)
+echo.
+echo All dependencies installed successfully!
 pause
